9a80e7a0f24a78ca3a963dcd1b15e0deaed0232a
Rudigus <rodrigo.matos9@hotmail.com>
2020-07-03 20:29:34 -0300

    initial xcode project files

---
+++ Color Cauldron/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Color Cauldron
+//
+//  Created by Rodrigo Matos Aguiar on 03/07/20.
+//  Copyright © 2020 Rudigus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
