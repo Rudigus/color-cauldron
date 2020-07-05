52c8494d9e5e6e4fb71542a4afcd00e4c0d30efb
Rudigus <rodrigo.matos9@hotmail.com>
2020-07-04 23:38:09 -0300

    added centralized color circle

--- Color Cauldron/View/DocumentView.swift
+++ Color Cauldron/View/DocumentView.swift
@@ -9,11 +9,41 @@
 import Cocoa
 
 class DocumentView: NSView {
-
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-
-        // Drawing code here.
+    
+    lazy var colorView: NSView = {
+        let colorView = NSView()
+        colorView.wantsLayer = true
+        colorView.layer?.backgroundColor = NSColor.red.cgColor
+        return colorView
+    }()
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        wantsLayer = true
+        layer?.backgroundColor = NSColor.windowBackgroundColor.cgColor
+        addSubview(colorView)
+        setupLayout()
+    }
+    
+    func setupLayout() {
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+        let circleRadius = NSScreen.main!.frame.size.width * 0.05
+        NSLayoutConstraint.activate([
+            colorView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            colorView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            colorView.widthAnchor.constraint(equalToConstant: circleRadius * 2),
+            colorView.heightAnchor.constraint(equalToConstant: circleRadius * 2)
+        ])
+        colorView.layoutSubtreeIfNeeded()
+        colorView.layer?.cornerRadius = colorView.frame.size.width / 2
     }
     
 }
